04d1658dcb8bc7aa24cdac63a5fc3f329e18d3ac
Zor <zormon@gmail.com>
2023-06-02 11:55:15 +0100

    removed search feature

--- resources/views/userlist.blade.php
+++ resources/views/userlist.blade.php
@@ -10,23 +10,15 @@
             <div class="alert alert-success" role="alert">{{ session('status') }}</div>
         @endif
 
-        <div class="input-group input-group-lg">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroup-sizing-lg">{{ __('Search') }}</span>
-            </div>
-            <input name="query" id="query" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
-        </div>
-
         <ul id="results" class="list-group"></ul>
     </div>
 
     <script>
-        var data = new Array({'query':''});
         $.ajax({
             type: "POST",
             url: "/users/s",
             headers: { 'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]').content },
-            data: JSON.stringify(data),
+            data: '[{"query":""}]',
             dataType: "json"
         }).done(( data )=> {
             $("#results").html("");
@@ -45,46 +37,6 @@
 
                 $("#results").append(li);
             });
-        });
-
-
-        $('#query').keyup((e)=> {
-            if($('#query').val().length > 3){
-                var data = new Array({'query':$('#query').val()});
-
-                $.ajax({
-                    type: "POST",
-                    url: "/users/s",
-                    headers: {
-                    'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]').content
-                },
-                    data: JSON.stringify(data),
-                    dataType: "json"
-                }).done(( data )=> {
-                    $("#results").html("");
-                    $(data).each((index, item)=> {
-                        $("#results").html('<li class="list-group-item list-group-item-action"><div class="row"><div class="col">' + item.name +'</div><div class="col"><small>' + item.email +'</small></div><div class="col"><a class="btn btn-primary" href="/users/edit/' + item.userId + '"><i class="fas fa-id-card"></i></a>&nbsp;<a class="btn btn-primary" href="/usrlist/' + item.userId + '"><i class="fas fa-video"></i></a></div></div></li>');
-                    });
-                });
-            }
-
-            if( $('#query').val().length == 0 ) {
-                var data = new Array({'query':''});
-
-                $.ajax({
-                    type: "POST",
-                    url: "/users/s",
-                    headers: { 'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]').content },
-                    data: JSON.stringify(data),
-                    dataType: "json"
-                }).done(( data )=> {
-                    $("#results").html("");
-
-                    $(data).each( (index, item)=> {
-                        $("#results").append('<li class="list-group-item list-group-item-action"><div class="row"><div class="col">' + item.name +'</div><div class="col"><small>' + item.email +'</small></div><div class="col"><a class="btn btn-primary" href="/users/edit/' + item.userId + '"><i class="fas fa-id-card"></i></a>&nbsp;<a class="btn btn-primary" href="/usrlist/' + item.userId + '"><i class="fas fa-video"></i></a></div></div></li>');
-                    });
-                });
-            }
         });
     </script>
     @endsection
