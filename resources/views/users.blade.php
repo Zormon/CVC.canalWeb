17eff8dbd83b1c144f0da493a1c4f3bf05e16dce
Zor <zormon@gmail.com>
2023-07-21 14:47:31 +0100

    cleaned language

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="card-header">{{ __("User's list") }}</div>
+<div class="card-header">{{ __("Userlist") }}</div>
 <div class="card-body">
     @if (session('status'))
         <div class="alert alert-success" role="alert">{{ session('status') }}</div>
